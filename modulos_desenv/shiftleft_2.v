cb2bcb924a109f4595c72d436b6c6087bf69daa2
Jeiel Victor <114118455+jeielv@users.noreply.github.com>
2025-02-12 17:13:25 -0300

    Add files via upload

---
+++ modulos_desenv/shiftleft_2.v
@@ -0,0 +1,6 @@
+module shiftleft_2 (
+    input  wire [31:0] in,
+    output wire [31:0] out,
+);
+    assign out = in << 2;
+endmodule
--- modulos_desenv/sign_extend_32.v
+++ modulos_desenv/sign_extend_32.v
@@ -1,6 +1,6 @@
 module extend_32(
     input  wire in,
-    output wire [31:0] data_out,
+    output wire [31:0] out,
 );
-    assign data_out = {{{31{1'b0}},in};
+    assign out = {{{31{1'b0}},in};
 endmodule
