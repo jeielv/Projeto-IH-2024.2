0762cd33d0e79b300a68290adc3ad5ad87eca793
Jeiel Victor <114118455+jeielv@users.noreply.github.com>
2025-02-13 15:23:16 -0300

    Create mux_2.v

---
+++ modulos_desenv/mux_2.v
@@ -0,0 +1,10 @@
+module mux_2(
+    input wire seletor
+    input wire [31:0] in_0
+    input wire [31:0] in_1
+    output wire [31:0] out
+);
+
+    assign out = (selector ? in_1 : in_0);
+
+endmodule
