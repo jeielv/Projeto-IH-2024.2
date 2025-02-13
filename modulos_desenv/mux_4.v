8100e4e8b1591b1eb91b802d62fbcf3ddc6c029b
Jeiel Victor <114118455+jeielv@users.noreply.github.com>
2025-02-13 15:20:40 -0300

    Update mux_4.v

--- modulos_desenv/mux_4.v
+++ modulos_desenv/mux_4.v
@@ -4,9 +4,9 @@ module mux_4(
     input wire [31:0] in_1     // A
     input wire [31:0] in_2     // B
     input wire [31:0] in_3     // ALU_Out
-    output wire [31:0] mem_addr
+    output wire [31:0] out
 );
 
-    assign mem_addr = (selector[1] ? (selector[0] ? in_3 : in_2) : (selector[0] ? in_1 : in_0))
+    assign out = (selector[1] ? (selector[0] ? in_3 : in_2) : (selector[0] ? in_1 : in_0))
 
 endmodule
