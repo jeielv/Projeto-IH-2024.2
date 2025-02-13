74ba0fd352ce0539b6b9a36ab2f2b8de2bf1eaeb
Jeiel Victor <114118455+jeielv@users.noreply.github.com>
2025-02-13 15:20:19 -0300

    Update mux_7.v

--- modulos_desenv/mux_7.v
+++ modulos_desenv/mux_7.v
@@ -7,7 +7,7 @@ module mux_7(
     input wire [31:0] in_4
     input wire [31:0] in_5 
     input wire [31:0] in_6 
-    output wire [31:0] mem_addr
+    output wire [31:0] out
 );
 
     assign mem_addr = (selector[2] ? ((selector[1] ? (selector[0] ? 32{1'b0} : in_6) : (selector[0] ? in_5 : in_4))) : ((selector[1] ? (selector[0] ? in_3 : in_2) : (selector[0] ? in_1 : in_0))))
