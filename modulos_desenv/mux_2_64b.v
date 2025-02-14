608abf46a26e4532dbc258c745f2c11c3d7c8246
Jeiel Victor <114118455+jeielv@users.noreply.github.com>
2025-02-14 19:39:23 -0300

    Update mux_2_64b.v

--- modulos_desenv/mux_2_64b.v
+++ modulos_desenv/mux_2_64b.v
@@ -1,7 +1,7 @@
 module mux_2_64b(
-    input wire seletor
-    input wire [63:0] in_0
-    input wire [63:0] in_1
+    input wire seletor,
+    input wire [63:0] in_0,
+    input wire [63:0] in_1,
     output wire [63:0] out
 );
 
