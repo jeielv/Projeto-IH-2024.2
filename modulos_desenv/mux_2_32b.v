e9193302fe8bf22f5b42594b4970b5155647639f
Jeiel Victor <114118455+jeielv@users.noreply.github.com>
2025-02-14 18:53:46 -0300

    Update mux_2_32b.v

--- modulos_desenv/mux_2_32b.v
+++ modulos_desenv/mux_2_32b.v
@@ -1,7 +1,7 @@
 module mux_2_32b(
-    input wire seletor
-    input wire [31:0] in_0
-    input wire [31:0] in_1
+    input wire seletor,
+    input wire [31:0] in_0,
+    input wire [31:0] in_1,
     output wire [31:0] out
 );
 
