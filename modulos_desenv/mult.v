80bf2a44203012512a926cc09b76cd01f75d33dd
Jeiel Victor <114118455+jeielv@users.noreply.github.com>
2025-02-14 19:39:48 -0300

    Update mult.v

--- modulos_desenv/mult.v
+++ modulos_desenv/mult.v
@@ -1,6 +1,6 @@
 module mult(
-    input wire [31:0] data_A
-    input wire [31:0] data_B
+    input wire [31:0] data_A,
+    input wire [31:0] data_B,
     output wire [63:0] out
 );
     assign out = data_A * data_B;
